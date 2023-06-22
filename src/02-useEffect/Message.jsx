39e5d6fda649940a75c7429be2cfca6c507d3432
Joshua <hola@joshua.cl>
2023-06-22 13:18:28 -0400

    Precauciones al usar useEffect

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,21 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Message = () => {
 
+  // useEffect(() => {
+
+  //   console.log('Message Mounted');
+
+  //   // El return en el useEffect sirve para desmontar/limpiar (unmount/cleanup) lo que acabo de disparar en el useEffect (Para probar bien este ejemplo de los console.logs desactivar el modo estricto de React)
+  //   return () => {
+  //     console.log('Message UnMounted');
+  //   }
+  // }, []);
+
+  const [coords, setCoords] = useState({ x: 0, y: 0});
+
   useEffect(() => {
 
-    console.log('Message Mounted');
-
-    // El return en el useEffect sirve para desmontar/limpiar (unmount/cleanup) lo que acabo de disparar en el useEffect (Para probar bien este ejemplo de los console.logs desactivar el modo estricto de React)
-    return () => {
-      console.log('Message UnMounted');
+    const onMouseMove = ({ x, y }) => {
+      setCoords({ x, y });
     }
-  }, [])
 
+    window.addEventListener( 'mousemove', onMouseMove );
+
+    return () => {
+      // Cuando asignamos comportamientos, en este caso un eventListener, siempre al desmontar el componente se debe de remover el comportamiento que se agregó
+      window.removeEventListener( 'mousemove', onMouseMove );
+    }
+  }, []);
 
   return (
     <>
       <h3>Usuario ya existe</h3>
+      { JSON.stringify( coords )}
     </>
   )
 }
